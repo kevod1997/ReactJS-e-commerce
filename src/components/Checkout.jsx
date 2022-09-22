9ee8979f7b888333d2fcb56890cdee3aaf8aaf38
Kevin Defalco <kevindefalco@gmail.com>
2022-09-22 20:09:15 -0300

    cambios en el layout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -10,9 +10,8 @@ export default function Checkout() {
   const [comprador, setComprador] = useState({});
   const [orderId, setOrderId] = useState("");
   const { cart, cartTotal, clear } = useCart();
-  const [loader, setLoader] = useState(false)
-  const navigate = useNavigate()
-
+  const [loader, setLoader] = useState(false);
+  const navigate = useNavigate();
 
   const datosComprador = (e) => {
     setComprador({
@@ -22,46 +21,49 @@ export default function Checkout() {
   };
 
   const finalizarCompra = (e) => {
-    if(Object.values(comprador).length !== 3){
-        e.preventDefault()
-        alert('Tenes que completar todos los campos')
-    }else{
-        e.preventDefault();
-        setLoader(true)
-        const ventas = collection(database, "orders");
-        addDoc(ventas, {
-          comprador,
-          items: cart,
-          total: cartTotal(),
-          date: serverTimestamp(),
+    if (Object.values(comprador).length !== 3) {
+      e.preventDefault();
+      alert("Tenes que completar todos los campos");
+    } else {
+      e.preventDefault();
+      setLoader(true);
+      const ventas = collection(database, "orders");
+      addDoc(ventas, {
+        comprador,
+        items: cart,
+        total: cartTotal(),
+        date: serverTimestamp(),
+      })
+        .then((res) => {
+          setOrderId(res.id);
+          clear();
         })
-          .then((res) => {
-            setOrderId(res.id);
-            clear();
-          })
-          .catch((error) => console.log(error))
-          .finally(()=> setLoader(false))
-      } 
+        .catch((error) => console.log(error))
+        .finally(() => setLoader(false));
     }
-    if(loader){
-        return <p><Loader/></p>
-    }
+  };
+  if (loader) {
+    return (
+      <p>
+        <Loader />
+      </p>
+    );
+  }
   return (
     <div>
-      {!orderId ? 
-        <div>
-          <h2>Finalizar Comprar</h2>
-          <h4>Por favor complete los siguientes campos</h4>
+      {!orderId ? (
+        <div className="card d-flex justify-content-center" style={{margin: '150px'}}>
+          <div>
+            <h3 className="d-flex justify-content-center">Finalizar Compra</h3>
+            <h4 className="d-flex justify-content-center">Por favor complete los siguientes campos</h4>
+          </div>
           <form
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
+          className="d-flex justify-content-center"
             onSubmit={finalizarCompra}
+            style={{marginBottom: '50px', marginTop: '30px'}}
           >
             <input
+            
               style={{ margin: "5px" }}
               type="text"
               placeholder="Ingresa tu nombre y Apellido"
@@ -69,6 +71,7 @@ export default function Checkout() {
               onChange={datosComprador}
             />
             <input
+            
               style={{ margin: "5px" }}
               type="number"
               placeholder="Ingresa tu telefono"
@@ -76,30 +79,42 @@ export default function Checkout() {
               onChange={datosComprador}
             />
             <input
+            
               style={{ margin: "5px" }}
               type="email"
               placeholder="Ingresa tu correo"
               name="email"
               onChange={datosComprador}
             />
-            <button type="submit">Comprar</button>
+            <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-outline-success d-flex justify-content-center">Comprar</button>
+            </div>
+            
           </form>
         </div>
-       : 
-        <div style={{
+      ) : (
+        <div
+          style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             flexDirection: "column",
-            backgroundColor: 'whitesmoke',
-            margin: '250px',
-            borderRadius: '10px',
-          }} >
-          <h2>Gracias por su compra!</h2>
-          <h4> Su orden es: {orderId} </h4>
-          <button className="btn btn-success" onClick={()=> navigate('/')} style={{margin:'10px'}} > Volver</button>
+            backgroundColor: "whitesmoke",
+            margin: "250px",
+            borderRadius: "10px",
+          }}
+        >
+          <h2>Gracias por comprar!</h2>
+          <button
+            className="btn btn-success"
+            onClick={() => navigate("/")}
+            style={{ margin: "10px" }}
+          >
+            {" "}
+            Volver
+          </button>
         </div>
-      }
+      )}
     </div>
   );
 }
